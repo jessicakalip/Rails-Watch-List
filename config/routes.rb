574afd82d419ab32b27a7a12caca78029ed75ffb
jessica kalip <jessicakalip@gmail.com>
2021-07-23 11:45:55 +0800

    Edit root_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'bookmarks/new'
+  root 'lists#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # LISTS
   resources :lists do
